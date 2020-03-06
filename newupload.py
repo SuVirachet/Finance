8057e3e61fe962e527d199e70d3917f195acdee3
SuVirachet <kozar.frozen@gmail.com>
2020-03-06 18:15:08 +0700

    Rest api version2.0
    Now code can connect with FXCM servers.

--- newupload.py
+++ newupload.py
@@ -1,17 +1,22 @@
 import fxcmpy
 import socketio
+import pandas as pd
+import datetime as dt
 
 print (fxcmpy.__version__)
-TOKEN = '67932040d63246639f24cc5c4170ad3dc5cd3b8d'
+TOKEN = '4b089f39a07dbb5f50d6a6a80c22aa81b055fe7e'
 # con = fxcmpy.fxcmpy(config_file='fxcm.cfg')
-con = fxcmpy.fxcmpy(config_file='fxcm.cfg', server='demo')
+con = fxcmpy.fxcmpy(access_token=TOKEN, log_level='error',server='demo')
+print(con.is_connected())
+print(con.get_instruments())
+instruments = con.get_instruments_for_candles()
+for i in range(int(len(instruments)/4)):
+    print(instruments[i*4:(i+1)*4])
+print(instruments[(i+1)*4:])
+print(con.get_candles('USD/JPY', period='D1'))
+
 # con = fxcmpy.fxcmpy(access_token = TOKEN, log_level = 'error')
 # instruments = con.get_instruments()
-# print(instruments[:5]) ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'EUR/CHF']
 # print(con.get_instruments())
 
 # print(con.get_instruments())
-# TOKEN = "c6436d54530a85bef95c5f4f61c003dde3f7a2ce"
-# con = fxcmpy.fxcmpy(access_token = TOKEN, log_level = 'error')
-# instruments = con.get_instruments()
-# print(instruments[:5]) ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'EUR/CHF']
